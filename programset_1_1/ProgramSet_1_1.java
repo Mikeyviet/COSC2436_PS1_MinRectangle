bddb7c3d38c69de886959bab34658ae5800177cd
Mikeyviet <vietwiin@gmail.com>
2022-09-26 16:14:25 -0500

    input writes

---
+++ programset_1_1/ProgramSet_1_1.java
@@ -0,0 +1,28 @@
+/**------------------------------------------------------------------------
+ * ?                                ABOUT
+ * @author         :  Michael Nguyen
+ * @email          :  michael.nguyen136448@my.tccd.edu
+ * @repo           :  
+ * @createdOn      :  September 12, 2022
+ * @description    :  Write a program that finds the minimal number of 
+ *  groupings in a matrix. The user will enter the size of the rows and 
+ *  columns in the range [2,8] from the keyboard. Error check input size. 
+ *  The program then will randomly fill the numbers 0 and 1 into the matrix 
+ *  and outputs to the screen the matrix and the minimum number of 
+ *  rectangles/squares that cover all the 1 values in the matrix. The size 
+ *  of a rectangle/square is defined by the number of 1's in it. Here are 
+ *  the rules in forming groups of ones: 
+ *------------------------------------------------------------------------**/
+package programset_1_1;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class ProgramSet_1_1 {
+    public static void main(String[] args) {
+        System.out.println("This is a program that finds the minimal number of groupings in a matrix.");
+
+
+    }
+}
