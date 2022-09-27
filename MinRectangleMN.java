ea934c45f0d26c9f136dde02c2340cc885c50dae
Mikeyviet <vietwiin@gmail.com>
2022-09-26 21:25:00 -0500

    Encapsulated Code with Loop

--- MinRectangleMN.java
+++ MinRectangleMN.java
@@ -28,33 +28,41 @@ public class MinRectangleMN {
         // Create a Scanner object for input
         var input = new Scanner(System.in);
 
-        // Prompt user for input
-        System.out.println("This is a program that does something.");
-        System.out.print("Enter the number of rows (2-10): ");
-        // read in the number of rows
-        int rows = input.nextInt();
-        System.out.print("Enter the number of columns (2-10): ");
-        // read in the number of columns
-        int columns = input.nextInt();
+        char choice = 'y';
 
-        // Create a 2D array
-        int[][] matrix = new int[rows][columns];
+        do{
+            // Prompt user for input
+            System.out.println("This is a program that does something.");
+            System.out.print("Enter the number of rows (2-10): ");
+            // read in the number of rows
+            int rows = input.nextInt();
+            System.out.print("Enter the number of columns (2-10): ");
+            // read in the number of columns
+            int columns = input.nextInt();
 
-        // Fill the array with random 0's and 1'size
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = (int) (Math.random() * 2);
+            // Create a 2D array
+            int[][] matrix = new int[rows][columns];
+
+            // Fill the array with random 0's and 1'size
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    matrix[i][j] = (int) (Math.random() * 2);
+                }
             }
-        }
-        // Print the array
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
+            // Print the array
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
-        
 
+            // Ask user if they want to continue
+            System.out.print("Do you want to continue? (y/n): ");
+            choice = input.next().charAt(0);
+
+        }while(choice != 'n');
+    
 
     }
 }
