4f531554595b254b66beb1ec5fc7fd5f11c960de
Mikeyviet <vietwiin@gmail.com>
2022-09-28 13:34:02 -0500

    Fixed x-axis grouping

--- MinRectangleMN.java
+++ MinRectangleMN.java
@@ -32,6 +32,8 @@ public class MinRectangleMN {
         int rows = 0;
         int columns = 0;
         int total = 0;
+        int countOfOnes = 0;   // Count of elements in row to check if all cells are 1
+
 
 
         do{
@@ -55,14 +57,18 @@ public class MinRectangleMN {
             boolean[][]flag = new boolean[rows][columns];
 
             // Fill the array with random 0's and 1'size
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix[i].length; j++) {
+            for (int i = 0; i < matrix.length; i++) 
+            {
+                for (int j = 0; j < matrix[i].length; j++) 
+                {
                     matrix[i][j] = (int) (Math.random() * 2);
                 }
             }
             // Print the array in matrix form
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix[i].length; j++) {
+            for (int i = 0; i < matrix.length; i++) 
+            {
+                for (int j = 0; j < matrix[i].length; j++) 
+                {
                     System.out.print(matrix[i][j] + " ");
                 }
                 System.out.println();
@@ -71,8 +77,10 @@ public class MinRectangleMN {
             System.out.println();
 
             // Print out the array values in MATRIX FORM with indices to find pattern
-            for(int i = 0; i < matrix.length; i++){
-                for(int j = 0; j < matrix[i].length; j++) {
+            for(int i = 0; i < matrix.length; i++)
+            {
+                for(int j = 0; j < matrix[i].length; j++) 
+                {
                     System.out.print("matrix[" + i + "][" + j + "] = " + matrix[i][j] + " ");
                 }
                 System.out.println();
@@ -81,32 +89,69 @@ public class MinRectangleMN {
             System.out.println();
 
             // Print out the array values in 1 row with indices to find pattern
-            for(int i = 0; i < matrix.length; i++){
-                for(int j = 0; j < matrix[i].length; j++) {
-                    System.out.println("matrix[" + i + "][" + j + "] = " + matrix[i][j] + " ");
-                }
-                System.out.println();
-            }
+            // for(int i = 0; i < matrix.length; i++)
+            // {
+            //     for(int j = 0; j < matrix[i].length; j++) 
+            //     {
+            //         System.out.println("matrix[" + i + "][" + j + "] = " + matrix[i][j] + " ");
+            //     }
+            //     System.out.println();
+            // }
 
             // Traverse the array and find all the 1's in the array
             for(int i = 0; i < matrix.length; i++){
-                for(int j = 0; j < matrix[i].length; j++) {
-                    if(matrix[i][j] == 1) {
-                        flag[i][j] = true; // 1 found, ready for vertical grouping
-                        if(j == 0) {
-                            total++; // 1 found in first column, count it
-                        } else if(matrix[i][j-1] == 0) {
-                            total++; // 1 found in column that is not first column and the column to the left is 0, count it
+                for(int j = 0; j < matrix[i].length; j++) 
+                {
+                    if(matrix[i][j] == 1) 
+                    {
+                        // 1 found, ready for vertical grouping
+                        countOfOnes++;
+                        flag[i][j] = true; 
+                        if(j == 0)
+                        {
+                            total++;
+                            System.out.println("Second Step: adding 1 found in 1st column. Total is now " + total);
+
+                        } 
+                        else if(matrix[i][j-1] == 0) 
+                        {
+                            total++;
+                            System.out.println("Third Step: adding one found in column. The total is now " + total); 
                         }
-                        // for the last cell in the column, if it has value of 1, need to check for value of 1 in first
-                        //     in the first column of current row
-                        if(j + 1 == matrix[i].length && matrix[i][j+1-columns] == 1) {
-                            total--; // 1 found in last column and 1 was also found in the first column so the total is reduced by 1
-                                     //     because the grouping will wrap around to the first column
+
+                        // if(i > 0)
+                        // {
+                        //     if(matrix[i-1][j] == matrix[i][j])
+                        //     {
+                        //         total--;
+                        //     }
+                        // }
+
+
+                    
+
+                        // For the last cell in the column, if it has value of 1, need to check for value of 1 in first
+                        // in the first column of current row
+                        if(j == matrix[i].length - 1 && matrix[i][0] == 1) 
+                        {
+                            // 1 found in last column and 1 was also found in the first column so the total is reduced by 1
+                            // because the grouping will wrap around to the first column
+                            if(countOfOnes != columns)
+                            {
+                                total--;
+                                System.out.println("Fourth Step: 1 Found in last column but subtracting previous one: " + total);
+                            }
+                            System.out.println("The number of columns is: " + columns);
+                            System.out.println("The total is now " + total);
+
+                            countOfOnes = 0;
+
+
                         }
-                            
-                        
+
+
                     }
+
                 }
             }
 
@@ -122,7 +167,5 @@ public class MinRectangleMN {
             System.out.println();
 
         }while(choice != 'n');
-    
-
     }
 }
