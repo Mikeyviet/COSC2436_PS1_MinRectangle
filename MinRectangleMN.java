734ff49cd49b9da10811df8c44c35bbbf0721b6c
Mikeyviet <vietwiin@gmail.com>
2022-09-27 19:48:32 -0500

    Counted groups of 1 horizontally

--- MinRectangleMN.java
+++ MinRectangleMN.java
@@ -29,21 +29,30 @@ public class MinRectangleMN {
         var input = new Scanner(System.in);
 
         char choice = 'y';
+        int rows = 0;
+        int columns = 0;
+        int total = 0;
+
 
         do{
             // Prompt user for input
             System.out.println("This is a program that does something.");
             System.out.print("Enter the number of rows (2-8): ");
             // read in the number of rows
-            int rows = input.nextInt();
+            rows = input.nextInt();
             System.out.print("Enter the number of columns (2-8): ");
             // read in the number of columns
-            int columns = input.nextInt();
+            columns = input.nextInt();
 
             System.out.println();
 
             // Create a 2D array
             int[][] matrix = new int[rows][columns];
+
+            // Create a 2D array to flag the columns if a 1 is found so that
+            // the column is not counted again and the 1 found in column will
+            // be grouped with the one directly above it
+            boolean[][]flag = new boolean[rows][columns];
 
             // Fill the array with random 0's and 1'size
             for (int i = 0; i < matrix.length; i++) {
@@ -80,15 +89,25 @@ public class MinRectangleMN {
             }
 
             // Traverse the array and find all the 1's in the array
-            // for(int i = 0; i < matrix.length; i++){
-            //     for(int j = 0; j < matrix[i].length; j++) {
-            //         if()
-            //     }
-            // }
+            for(int i = 0; i < matrix.length; i++){
+                for(int j = 0; j < matrix[i].length; j++) {
+                    if(matrix[i][j] == 1) {
+                        flag[i][j] = true; // 1 found, ready for vertical grouping
+                        if(j == 0) {
+                            total++; // 1 found in first column, count it
+                        } else if(matrix[i][j-1] == 0) {
+                            total++; // 1 found in column that is not first column and the column to the left is 0, count it
+                        }
+                    }
+                }
+            }
 
+            // Test print the total of 1's found horizontally grouped only in array
+            System.out.println("The total number of 1's found grouped horizontally is: " + total);
+            // Reset total to 0 for next run
+            total = 0;
 
-
-                System.out.println();
+            System.out.println();
             // Ask user if they want to continue
             System.out.print("Do you want to continue? (y/n): ");
             choice = input.next().charAt(0);
